07689262441144eee294fea49e63767109f9a0ca
GoogleyEyes <froginapond10@gmail.com>
2026-09-17 15:10:29 +1000

    Restart button UI

--- marble-game/Assets/Scripts/LevelManager.gd
+++ marble-game/Assets/Scripts/LevelManager.gd
@@ -4,3 +4,8 @@ extends Node2D
 func _on_menu_button_pressed() -> void:
 	# This is where the options menu / pause menu will go
 	print("Menu button pressed")
+
+
+func _on_restart_button_pressed() -> void:
+	# This is where the code to restart the level will go
+	print("Restart button pressed")
