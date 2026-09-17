3c6d9bafff268897c4edc1425507505cd86a4a2d
GoogleyEyes <froginapond10@gmail.com>
2026-09-17 10:27:21 +1000

    Basic platform select UI

---
+++ marble-game/Scenes/Platform.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var is_grabbed: bool = false
+
+
+func _process(delta: float) -> void:
+	if is_grabbed:
+		global_position = get_global_mouse_position()
+
+
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				is_grabbed = true
+			else:
+				is_grabbed = false
