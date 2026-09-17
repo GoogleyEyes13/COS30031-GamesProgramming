a6d7581b81c0c1ff8fdbbb337d00598a8d7d21d2
GoogleyEyes <froginapond10@gmail.com>
2026-09-17 16:30:01 +1000

    Background grid

--- marble-game/Assets/Scripts/LevelManager.gd
+++ marble-game/Assets/Scripts/LevelManager.gd
@@ -2,8 +2,37 @@ extends Node2D
 
 @onready var PlatformEditor = $PlatformEditor
 
+# Setting grid size
+var GridColumns: int = 24
+var GridSize: float
+
+
 func _ready() -> void:
 	PlatformEditor.visible = true
+
+	var ScreenSize = get_viewport_rect().size
+	GridSize = ScreenSize.x / GridColumns
+
+	queue_redraw()
+
+
+func _draw() -> void:
+	var ScreenSize = get_viewport_rect().size
+
+	# Vertical lines
+	for x in range(GridColumns + 1):
+		var x_position = x * GridSize
+
+		draw_line(Vector2(x_position, 0), Vector2(x_position, ScreenSize.y), Color(0.337, 0.173, 0.0, 0.667), 1.0)
+
+	# Horizontal lines
+	var rows = int(ceil(ScreenSize.y / GridSize))
+
+	for y in range(rows + 1):
+		var y_position = y * GridSize
+
+		draw_line(Vector2(0, y_position), Vector2(ScreenSize.x, y_position), Color(0.337, 0.173, 0.0, 0.667), 1.0)
+
 
 func _on_menu_button_pressed() -> void:
 	# This is where the options menu / pause menu will go
