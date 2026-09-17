d52f20588e297eec9ea303ab866ca67876419852
GoogleyEyes <froginapond10@gmail.com>
2026-09-17 16:12:59 +1000

    Platform editor and exit button

--- marble-game/Assets/Scripts/LevelManager.gd
+++ marble-game/Assets/Scripts/LevelManager.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var PlatformEditor = $PlatformEditor
+
+func _ready() -> void:
+	PlatformEditor.visible = true
 
 func _on_menu_button_pressed() -> void:
 	# This is where the options menu / pause menu will go
@@ -9,3 +13,9 @@ func _on_menu_button_pressed() -> void:
 func _on_restart_button_pressed() -> void:
 	# This is where the code to restart the level will go
 	print("Restart button pressed")
+
+
+func _on_exit_button_pressed() -> void:
+	# Closing the Platform editor
+	PlatformEditor.visible = false
+	print("Platform editor exited")
